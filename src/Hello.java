0236f50d55fad3f7e173f2dd2d57ba5b525fa4ab
tactreconx <league@WTS-MM-11.local>
2017-04-15 11:34:07 -0700

    Learned to write messages

---
+++ src/Hello.java
@@ -0,0 +1,16 @@
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello World, my name is Neo");
+		int age = 15;
+		System.out.println("I am " + age + " years old");
+		int nextyear = age + 1;
+		System.out.println("Next year I will be " + nextyear + " years old");
+		int lastyear = age - 1;
+		System.out.println("Last year I was " + lastyear + " years old");
+		String school = "CVLCC";
+		System.out.println("I go to " + school);
+		String whatIwantToBe = "Software Engineer";
+		System.out.println("When I'm older I want to be a " + whatIwantToBe);
+	}
+}
