d9aea644076b7686e37e5fcbd5e7fb7e152af9d3
EmperorlNeo <league@WorthyMini.local>
2017-08-05 11:35:57 -0700

    update 2

--- src/Code4Life.java
+++ src/Code4Life.java
@@ -6,6 +6,9 @@ import javax.sound.sampled.Clip;
 import javax.swing.JOptionPane;
 
 public class Code4Life {
+	public static void main(String[] args) {
+		
+	
 	//Ask the user how many hours they spent coding this week.
 	String answer=JOptionPane.showInputDialog(null,"How many hours have you spent coding this week?");
 	 // 1. If it's 3 or more, tell them they're a Code Ninja.
@@ -33,4 +36,5 @@ public class Code4Life {
 		}
 	}
 
+}
 }
