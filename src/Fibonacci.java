99bdb98649b368afb4d26048f437a5f3060d0b98
tactreconx <league@Admins-Mac-mini.local>
2017-11-18 11:30:47 -0800

    Checkpoints
    Checkpoints

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,16 +1,9 @@
 
 public class Fibonacci {
 	public static void main(String[] args) {
-		int x = 0;
-		int r = 1;
-		int i;
-		int f;
+		for(int i = 0; i<=144; i++){
 		System.out.println();
-		for (i = 0; i <= 144; i++) {
-			f = i + r;
-			x = f + r;
-			i = x + f;
-			System.out.println(f);
+		
 		}
 	}
 
