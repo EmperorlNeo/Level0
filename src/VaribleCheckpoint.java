f5f938fe3e5e538b780c6fe07a8cb4a2502fa25c
tactreconx <league@Admins-Mac-mini.local>
2017-11-18 11:31:38 -0800

    Variable checkpoint
    Variables

--- src/VaribleCheckpoint.java
+++ src/VaribleCheckpoint.java
@@ -1,4 +1,48 @@
 
 public class VaribleCheckpoint {
-//
+// string favoriteHobby = "programming";
+
+
+// int numberOfToes = 10;
+
+
+// int degreesInARightAngle = 90;
+
+
+// int myAge = 16 ;
+
+
+//String codeLanguage = "Java";
+
+
+//double pi = 3.14159;
+	
+	
+	//String whatIWantToBeWhenIGrowUp = "Software Programmer";
+
+   // int facebookFriends = 500;
+
+   // String myBirthday = "August 27,2001";
+
+   // int sidesInATriangle = 3;
+	
+	
+	
+	// - what you got for your birthday
+//String BirthdayGifts = "steak";
+
+
+
+	//    - the last meal you ate
+//String LastMeal = "Quesadilla";
+
+
+
+	  //  - what time you go to bed
+// String BedTime = "1 am";
+
+
+
+	 //   - how many Robot programs you have written
+// int NumberOfRobots = 0;
 }
