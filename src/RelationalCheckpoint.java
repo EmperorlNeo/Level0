c1e0284d18f90f4028e1f36fd192d0db9913201c
EmperorlNeo <league@Mac-mini.local>
2017-12-09 10:37:49 -0800

    Relational checkpoint
    module 3 checkpoint

--- src/RelationalCheckpoint.java
+++ src/RelationalCheckpoint.java
@@ -8,7 +8,20 @@ public static void main(String[] args) {
 		JOptionPane.showMessageDialog(null, "Go get some more I guess, idk");
 	}
 	else{
-		JOptionPane.showMessageDialog(null, "Why? What are you even doing");
+		JOptionPane.showMessageDialog(null, "Why do you need so much flour?");
+	}
+	String c=JOptionPane.showInputDialog("How many people are you going to give cookies to?");
+	int d= Integer.parseInt(c);
+	if(d>=30){	
+		JOptionPane.showMessageDialog(null, "Then you'll have to bake two batches of cookies!");
+	}
+	else{
+		JOptionPane.showMessageDialog(null, "You only need to bake one batch.");
+	}
+	String e=JOptionPane.showInputDialog("How many batches of cookies did you bake?");
+	int f= Integer.parseInt(e);
+	if(f<=2){
+		JOptionPane.showMessageDialog(null, "Wow, you have a lot of friends");
 	}
 }
 }
