4272a1854adce05726de410a5b75f41c5506b223
EmperorlNeo <league@FunkyMini.local>
2017-12-23 11:27:11 -0800

    Module 2 checkpoints again
    Random Checkpoint again

---
+++ src/RandomCheckpoint2.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class RandomCheckpoint2 {
+	public static void main(String[] args) {
+
+//		int a = new Random().nextInt(1000);
+//		System.out.println(a);
+//		int i = new Random().nextInt(900);
+//		System.out.println(i);
+		int c=99;
+		int b= 64;
+		System.out.println(c-b);
+}
+}
