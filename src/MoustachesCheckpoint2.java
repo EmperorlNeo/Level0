eb0e5a47a5cf35542a372ae54eea4f2dd2e2c5e2
EmperorlNeo <league@FunkyMini.local>
2017-12-23 10:41:11 -0800

    Module 1 Checkpoint again
    Mustaches Checkpoint again

---
+++ src/MoustachesCheckpoint2.java
@@ -0,0 +1,16 @@
+public class MoustachesCheckpoint2{
+
+    static int tortoiseLocation;{
+
+    public static void main(String[] args) {
+
+
+        tortoiseLocation = Tortoise.getLocation();
+
+
+        if (tortoiseLocation < 0) 
+
+              System.out.println("El Tortuga se ha ido!");
+}
+}
+}
