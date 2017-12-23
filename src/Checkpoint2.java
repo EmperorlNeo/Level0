915d4bdcd80ef400de210c1937f0f36099a511e5
EmperorlNeo <league@FunkyMini.local>
2017-12-23 10:14:03 -0800

    Redid module 0 checkpoint

---
+++ src/Checkpoint2.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class Checkpoint2 {
+public static void main(String[] args) {
+	String a=JOptionPane.showInputDialog(null, "What's your favorite color?");
+	JOptionPane.showMessageDialog(null, a +" is my favorite color too.");
+	
+}
+}
