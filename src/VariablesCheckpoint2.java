345694882ce15c22c925cc6423d7f4c6e66194fc
EmperorlNeo <league@FunkyMini.local>
2017-12-23 10:27:19 -0800

    Module 1 Checkpoint again
    Variables Checkpoint 2

---
+++ src/VariablesCheckpoint2.java
@@ -0,0 +1,20 @@
+
+public class VariablesCheckpoint2 {
+	//1)
+// String favoriteHobby = "programming";
+// int numberOfToes = 10;
+// int degreeInARightTriangle = 90;
+// int myAge = 16;
+// String ComputerLanguage = "Java";
+// double pi = 3.14159;
+	//2)
+// String whatIwantToBeWhenIGrowUp = "Software Engineer";
+// int facebookFriends = 300;
+// String myBirthday = "August 27th";
+// int sidesInATriangle = 3;
+	//3)
+// String WhatIGotForMyBirthday= "sleep";
+// String LastMealIAte = "Scrambled Eggs";
+// int BedTime = 11;
+// int NumberOfRobotPrograms = 1;
+}
