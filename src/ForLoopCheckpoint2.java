fe62f1d97b72224354cc0474e58774873c635854
EmperorlNeo <league@FunkyMini.local>
2017-12-23 10:37:13 -0800

    Module 1 checkpoints again
    For loop Checkpoint 2

---
+++ src/ForLoopCheckpoint2.java
@@ -0,0 +1,17 @@
+
+public class ForLoopCheckpoint2 {
+public static void main(String[] args) {
+	//1)
+//	for(int i=1; i<=10;i++){
+//		System.out.println(i);
+//	}
+	//2)
+//	for(int a=75; a>=50; a--){
+//		System.out.println(a);
+//	}
+	//3)
+// a=The initial point/number of the for loop
+// b=Where the number will end up
+// c=If you need to subtract or add to get from the initial number to the end number.
+}
+}
