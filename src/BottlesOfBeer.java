3c4739c4c83a3cf357dfb7d79f46d5612c126f50
EmperorlNeo <league@WorthyMini.local>
2018-01-27 11:23:09 -0800

    99 bottles of beer

--- src/BottlesOfBeer.java
+++ src/BottlesOfBeer.java
@@ -1,4 +1,14 @@
+import javax.swing.JOptionPane;
 
 public class BottlesOfBeer {
-
+public static void main(String[]args){
+	String a= " bottle";
+	String b= " on the wall";
+	String c= "Take one down and pass it around, ";
+	String d= " of beer";
+	for(int i=99; i>=3; i--){
+		System.out.println(i+a+"s"+d+b+", "+i+a+"s"+d+"."+c+(i-1)+a+"s"+d+b+".");
+	}
+	System.out.println();
+}
 }
