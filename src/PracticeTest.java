9292c16af5348a692a35e5ee37d93e5c56e69e29
EmperorlNeo <league@WorthyMini.local>
2018-01-27 11:23:38 -0800

    Practice test part 2
    code4Life

---
+++ src/PracticeTest.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
+
+public class PracticeTest {
+	public static void main(String[]args){
+	/*
+	 * Ask the user how many hours they spent coding this week.
+	 *
+	 * 1. If it's 3 or more, tell them they're a Code Ninja.
+	 *
+	 * 2. If it's less than 2, tell them to stop watching YouTube and write code
+	 * instead.
+	 *
+	 * 3. If it's more than 5, play the Batman theme song.
+	 */
+	String a = JOptionPane.showInputDialog("How many hours have you spent coding this week?");
+	int b=Integer.parseInt(a);
+	if(b>=5){
+		playBatmanTheme();
+	}
+	else if(b>=3){
+		JOptionPane.showMessageDialog(null, "You're a Code Ninja!");
+	}
+	else if(b<=2){
+		JOptionPane.showMessageDialog(null, "Stop watching Youtube and write some code instead");
+	}
+	
+	}
+private static void playBatmanTheme() {
+
+	try {
+AudioInputStream audioInputStream =
+AudioSystem.getAudioInputStream(new File("/Users/League/Google Drive/league-sounds/batman.wav"));
+Clip clip = AudioSystem.getClip();
+clip.open(audioInputStream);
+clip.start();
+Thread.sleep(60002);
+} 
+	catch (Exception ex) {
+ex.printStackTrace();
+}
+
+}
+}
+	
